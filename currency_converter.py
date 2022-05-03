811df36bf2483beb4521c845a58247aca1e0687e
Zach Rickers <mzrickers@gmail.com>
2022-05-03 08:35:49 +0200

    get requests

--- currency_converter.py
+++ currency_converter.py
@@ -5,3 +5,12 @@ BASE_URL = "https://free.currconv.com/"
 API_KEY = "adabd5ff5f2b5b5b0338"
 
 printer = PrettyPrinter()
+
+def get_currencies():
+  endpoint = f"api/v7/currencies?apiKey={API_KEY}"
+  url = BASE_URL + endpoint
+  data = get(url).json()
+
+  printer.pprint(data)
+
+get_currencies()
